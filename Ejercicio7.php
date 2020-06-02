83906950a74b637b34f78f762294222498599f08
katerine3110 <katerinesabillondiaz@gmail.com>
2020-06-02 13:15:29 -0600

    Update Ejercicio7.php

--- Ejercicio7.php
+++ Ejercicio7.php
@@ -24,7 +24,7 @@
              echo"<p>Es primo.</p>";}
 
           else {
-            echo"<p>No es primo $cuenta</p>" ;}
+            echo"<p>$numero No es primo </p>" ;}
                 
         
         
